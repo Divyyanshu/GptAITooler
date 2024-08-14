36e06fa616cf3d00a2ca04b3bbc825db0af3e80c
Divyyanshu <divvyyanshuutailor@gmail.com>
2024-08-15 00:05:39 +0530

    add theme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,27 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
+import { useMemo } from 'react'
+import {CssBaseline,ThemeProvider} from "@mui/material";
+import { createTheme } from '@mui/material/styles'
+import {themeSettings} from './theme'
 import Homepage from './pages/Homepage'
 import Navbar from './components/Navbar'
 import Register from './pages/Register'
 import Login from './pages/Login'
+
 function App() {
+  const theme = useMemo(()=>createTheme(themeSettings(), []))
   return (
     <>
-      <Navbar/>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+    <Navbar/>
       <Routes>
         <Route path='/' element ={<Homepage/>}/>
         <Route path='/register' element ={<Register/>}/>
         <Route path='/login' element ={<Login/>}/>
       </Routes>
+    </ThemeProvider>
     </>
   )
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import {Box , Link  , Typography} from "@mui/material"
-const Navbar = () => {
-  return (
-    <Box width ={"100"} p= "1rem 6%" textAlign={'center'} sx={{boxShadow:4 ,mb:2 ,bgcolor:"purple",color:white}}>
-      <Typography variant="h1" color="initial">
-        AI-Gpt
-      </Typography>
-    </Box>
-  )
-}
+import { Box, Link, Typography, useTheme} from "@mui/material";
 
-export default Navbar
+
+const Navbar = () => {
+  const theme = useTheme()
+  return (
+    <Box
+      width={"100"}
+      backgroundColor = {theme.palette.background.alt}
+      p="1rem 6%"
+      textAlign={"center"}
+      sx={{ boxShadow: 3, mb: 2}}
+    >
+      <Typography variant="h1" color={"primary"} fontWeight="bold">
+        AI-Gpt Cloner
+      </Typography>
+      <Link href="/register" p={1}>
+        Sign-up
+      </Link>
+      <Link href="/login" p={1}>
+        Login
+      </Link>
+    </Box>
+  );
+};
+
+export default Navbar;
