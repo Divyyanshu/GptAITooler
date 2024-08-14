9b1c6bf815b8baa48d67615c76fce419abc588cf
Divyyanshu <divvyyanshuutailor@gmail.com>
2024-08-14 19:18:21 +0530

    react routing done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
 import './App.css'
-
+import { Routes, Route } from 'react-router-dom'
+import Homepage from './pages/Homepage'
+import Navbar from './components/Navbar'
+import Register from './pages/Register'
+import Login from './pages/Login'
 function App() {
   return (
     <>
-      <h1>Welcome TO ChatGpt-Latest</h1>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element ={<Homepage/>}/>
+        <Route path='/register' element ={<Register/>}/>
+        <Route path='/login' element ={<Login/>}/>
+      </Routes>
     </>
   )
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import {Box , Link  , Typography} from "@mui/material"
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Box width ={"100"} p= "1rem 6%" textAlign={'center'} sx={{boxShadow:4 ,mb:2 ,bgcolor:"purple",color:white}}>
+      <Typography variant="h1" color="initial">
+        AI-Gpt
+      </Typography>
+    </Box>
   )
 }
 
